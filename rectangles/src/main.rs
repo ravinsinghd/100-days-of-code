f57f41aa78264572325b3773dbffff6f370a1193
Ravin Singh D <ravin@getcloudcherry.com>
2019-08-24 19:52:10 +0530

    update function to accept struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,9 +1,17 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 fn main() {
-    let width = 30;
-    let height = 50;
-    println!("The area of the rectangle is {}", area(width, height))
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("{:#?}", rect1);
+    println!("The area of the rectangle is {}", area(rect1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
