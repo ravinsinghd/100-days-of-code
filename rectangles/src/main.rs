a245cc6a8334487191150ea8747e0299aa227a9f
Ravin Singh D <ravin@getcloudcherry.com>
2019-08-26 10:23:11 +0530

    struct methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,15 +3,21 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
     println!("{:#?}", rect1);
-    println!("The area of the rectangle is {}", area(rect1));
-}
-
-fn area(rectangle: Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle is {}", rect1.area());
 }
